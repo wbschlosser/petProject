99040df32274535d3e32d3e7e8dd008c46474af6
wbschlosser <wesleysemail37@yahoo.com>
2014-10-29 23:16:52 -0500

    Layout update
    Updated the banner with placeholder images and added functionality for
    an accordion style interface.

---
+++ script.js
@@ -0,0 +1,73 @@
+var images= new Array()
+images[0]="images/junkoMax.jpg";
+images[1]="images/junkoZero.jpg";
+images[2]="images/letgo.jpg";
+images[3]="images/comrades.jpg";
+setInterval("changeImage()",5000);
+var fade = true;
+var x=0;
+function changeImage(){
+	document.getElementById("testing").src=images[x]
+	//fadeImg(images[x], 100, true);
+	//setInterval("changeImage()",5000);
+	x++;
+	if(x >= images.length){
+		x=0;
+	}
+}
+/*
+function fadeImg(el, val, fade){
+	if(fade===true){
+		val--;
+	}
+	else{
+		val++;
+	}
+	if(val > 0 && val < 100){
+		el.style.opacity = val/100;
+		setTimeout(function(){fadeImg(el,val,fade);},10);
+	}
+}
+*/
+
+/*Accordion magic*/
+var accordionItems = new Array();
+function init() {
+
+    // Grab the accordion items from the page
+    var divs = document.getElementsByTagName( 'div' );
+    for ( var i = 0; i < divs.length; i++ ) {
+		if ( divs[i].className == 'accordionItem' ) accordionItems.push( divs[i] );
+    }
+
+      // Assign onclick events to the accordion item headings
+    for ( var i = 0; i < accordionItems.length; i++ ) {
+        var h2 = getFirstChildWithTagName( accordionItems[i], 'H2' );
+        h2.onclick = toggleItem;
+    }
+
+      // Hide all accordion item bodies except the first
+    for ( var i = 1; i < accordionItems.length; i++ ) {
+        accordionItems[i].className = 'accordionItem hide';
+    }
+}
+
+function toggleItem() {
+	var itemClass = this.parentNode.className;
+
+	// Hide all items
+	for ( var i = 0; i < accordionItems.length; i++ ) {
+		accordionItems[i].className = 'accordionItem hide';
+	}
+	
+	// Show this item if it was previously hidden
+	if ( itemClass == 'accordionItem hide' ) {
+		this.parentNode.className = 'accordionItem';
+	}
+}
+
+function getFirstChildWithTagName( element, tagName ) {
+	for ( var i = 0; i < element.childNodes.length; i++ ) {
+		if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
+	}
+}

18c5df9e154c3650b0cd7a1e115fe6bcb20586ae
seusch <saraeusch@Sara-Euschs-MacBook-Pro.local>
2014-11-18 17:56:05 -0600

    nav script

--- script.js
+++ script.js
@@ -72,3 +72,26 @@ function getFirstChildWithTagName( element, tagName ) {
 		if ( element.childNodes[i].nodeName == tagName ) return element.childNodes[i];
 	}
 }
+
+/*Navigation script*/
+$(window).resize(function(){
+		var windowheight=$(window).height();
+		$(".mainsection").height(windowheight);
+	}); 
+	$(window).resize();
+	
+	$("#replacement a").click(function(evt){
+		evt.preventDefault();
+		var sectionname = $(this).data("section");
+		scroll_offset = $('#'+sectionname).offset(),
+		scroll_offset_top = scroll_offset.top;
+		
+		$("body, html").animate({
+			"scrollTop": scroll_offset_top
+		});
+		function overlay() {
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+		}
+	});
+	
